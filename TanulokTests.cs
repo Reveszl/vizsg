b08091bd284138e07bae38aa869dee5d86de2d99
Reveszl <94444342+Reveszl@users.noreply.github.com>
2023-04-27 11:16:46 +0200

    Add files via upload

---
+++ TanulokTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Iskola1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iskola1.Tests
+{
+    [TestClass()]
+    public class TanulokTests
+    {
+        [TestMethod()]
+        public void KodTestJo()
+        {
+            Assert.AreEqual("6cbodszi", Tanulok.Kod(2006, "c", "Bodnar Szilvia"));
+        }
+
+        [TestMethod()]
+        public void KodTestRossz()
+        {
+            Assert.AreEqual("5abodszi", Tanulok.Kod(2006, "c", "Bodnar Szilvia"));
+        }
+    }
+}
